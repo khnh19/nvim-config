89ddcb571bf9abd3c215e192e250d9075945a132
Khanh Nguyen <khnhn1901@gmail.com>
2025-07-12 20:53:34 +0700

    feat(lspsaga): Disable breadcrumbs

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -10,6 +10,9 @@ return {
       lightbulb = {
         enable = false,
       },
+      symbol_in_winbar = {
+        enable = false,
+      },
     }
 
     map("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<cr>")
