2e60c862acf973859a491697d48c227f999d0f03
Khanh Nguyen <khnhn1901@gmail.com>
2025-08-10 22:01:03 +0700

    feat: Disable `lazy` reload notification

--- init.lua
+++ init.lua
@@ -15,6 +15,11 @@ vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
 
-require("lazy").setup "plugins"
+require("lazy").setup("plugins", {
+  change_detection = {
+    notify = false,
+  },
+})
+
 require "base"
 require "keymap"
